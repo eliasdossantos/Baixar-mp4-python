56c0e043b3ad808f81d20d01aa55fe7557dd786f
Elias Antonio <105184013+eliasdossantos@users.noreply.github.com>
2022-11-18 13:31:24 -0300

    Add files via upload

---
+++ baixar-mp4.py
@@ -0,0 +1,19 @@
+
+from pytube import YouTube, streams
+from pytube.cli import on_progress
+
+# Instalação das biblioteca do python pelo cmd ou o PowerShell com os comandos abaixo
+# pip install pytube
+# pip install moviepy
+# pip shell
+
+# Digite o link do video no caso a (url) do video do youtube
+link = input("Digite o link do video que deseja baixar: ")
+yt = YouTube(link, on_progress_callback = on_progress)
+print("Titulo = ", yt.title)
+print("Baixando... ")
+
+# Vai baixar a maior resolução do video que pretende baixer disponivel do video
+ys = yt.streams.get_highest_resolution()
+ys.download()
+print("Download completo! ")
